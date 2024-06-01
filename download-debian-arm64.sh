ee7ef1d08e4249a2324bac6d904337b3fbdac12a
d3rt0xx <98334773+d3rt0xx@users.noreply.github.com>
2024-06-02 01:41:51 +0200

    Update download link

--- download-debian-arm64.sh
+++ download-debian-arm64.sh
@@ -1,1 +1,1 @@
-wget -O debian-original.iso wget -O debian-original.iso https://cdimage.debian.org/debian-cd/current/arm64/iso-cd/debian-12.1.0-arm64-netinst.iso
+wget -O debian-original.iso wget -O debian-original.iso https://cdimage.debian.org/debian-cd/current/arm64/iso-cd/debian-12.5.0-arm64-netinst.iso
