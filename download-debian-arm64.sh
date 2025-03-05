b6dde584f0b744786367a31ac824efbabc5b9363
Jean28518 <jf_v@gmx.de>
2025-03-05 12:02:54 +0000

    Update download script to fetch the latest Debian ARM64 ISO from the correct source

--- download-debian-arm64.sh
+++ download-debian-arm64.sh
@@ -1,9 +1,9 @@
 # Download the index.html of debian.org
-wget -O /tmp/debian-index.html https://www.debian.org/distrib/
+wget -O /tmp/debian-index.html https://cdimage.debian.org/debian-cd/current/arm64/iso-cd/
 
 #search for the line with https://*-amd64-netinst.iso and extract the link to the latest debian iso
 DOWNLOAD_LINK=$(grep -oP 'href="\K[^"]*debian-[0-9]+.[0-9]+.[0-9]+-arm64-netinst.iso' /tmp/debian-index.html)
 
 echo "Downloading the latest debian iso from: $DOWNLOAD_LINK"
 
-wget -O debian-original.iso $DOWNLOAD_LINK
+wget -O debian-original.iso https://cdimage.debian.org/debian-cd/current/arm64/iso-cd/$DOWNLOAD_LINK
